6ea6cd374c30d1bf53a6d9b9ab31c0d99c416d3e
Ruaan Kruger <ruaankruger@gmail.com>
2022-05-03 21:00:24 +0200

    .emulator_console_auth_token reader

---
+++ src/Chapmans.Peak/Emulator/AuthTokenReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Chapmans.Peak.Emulator;
+
+/// <summary>
+/// In order to correctly establish a connection to the emulator, we need to authenticate the connection
+/// by reading the content of the .emulator_console_auth_token located in %USERPROFILE% (Windows) or $HOME (*nix)
+/// </summary>
+public static class AuthTokenReader
+{
+    private const string TokenFileName = ".emulator_console_auth_token";
+
+    /// <summary>
+    /// Read the token file from the user home folder
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="FileNotFoundException"></exception>
+    public static async Task<string> GetToken()
+    {
+        var userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var tokenPath = Path.Combine(userProfilePath, TokenFileName);
+
+        if (File.Exists(tokenPath))
+        {
+            throw new FileNotFoundException($"Unable to locate authentication file : {tokenPath}");
+        }
+        
+        return await File.ReadAllTextAsync(tokenPath, Encoding.Default);
+    }
+
+}
